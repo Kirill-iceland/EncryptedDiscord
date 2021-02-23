a43f33ce0bca85ec240a5b914c6a2c990d44a609
Kirill-iceland <58794914+Kirill-iceland@users.noreply.github.com>
2021-02-23 14:18:25 +0000

    Update

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 var fc = require('./src/build/Feistel_Cipher.js')
+var fs = require('fs')
+var Discord = require('discord.js')
+var commands = require('./src/build/commands.js')
+
+//{"token": "12345", "channels": [], "names": [], "keys": []}
+
+const bot = new Discord.Client()
+
+fs.readFile('config.json', (err, config) => {
+    if (err) return commands.getConfig(config => {
+        bot.login(config.token)
+    })
+    config = JSON.parse(config)
+    bot.login(config.token)
+});
+
 console.log(fc.encrypt('lol', '1234567890'))
